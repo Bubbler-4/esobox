5773bdd7f3141f0661cdf0ea18fad9a21e9d1221
Bubbler-4 <bubbler9903@gmail.com>
2022-06-22 06:42:29 +0000

    Clippy, fmt, doc, tests for bf

--- src/brainfuck/mod.rs
+++ src/brainfuck/mod.rs
@@ -1,20 +1,34 @@
+//! An implementation of [Brainfuck].
+//!
+//! This implementation uses a memory tape of fixed length (30000) with 8-bit wrapping cells.
+//! On EOF, `,` command does not modify the current cell.
+//!
+//! Since optimizing Brainfuck is a well-studied area and there are various extremely
+//! performant implementations out there, this one mostly serves as a practice implementation
+//! of a naive bytecode interpreter.
+//!
+//! [Brainfuck]: https://esolangs.org/wiki/Brainfuck
+
 use std::io::{self, BufRead, Write};
 use thiserror::Error;
 
+/// Error enum for Brainfuck.
 #[derive(Error, Debug)]
 pub enum Error {
+    /// Syntax error; the source code contains unmatched brackets.
     #[error("unmatched bracket `{0}`")]
     SyntaxError(char),
+    /// Runtime error; the pointer moved out of bounds of the tape (either to the left or to the right).
     #[error("pointer out of bounds on `{0}`")]
     PointerOutOfBoundsError(char),
+    /// I/O error, which may occur during I/O operations.
     #[error("unexpected I/O error")]
     IoError(#[from] io::Error),
-    #[error("unknown error")]
-    UnknownError,
 }
 
 const MEMORY_SIZE: usize = 30000;
 
+/// Brainfuck interpreter.
 pub fn run<I: BufRead, O: Write>(source: &str, input: &mut I, output: &mut O) -> Result<(), Error> {
     let basic_blocks = into_basic_blocks(source)?;
     let mut bb_no = 0usize;
@@ -26,9 +40,21 @@ pub fn run<I: BufRead, O: Write>(source: &str, input: &mut I, output: &mut O) ->
             match instr {
                 Cmd::Inc => memory[ptr] = memory[ptr].wrapping_add(1),
                 Cmd::Dec => memory[ptr] = memory[ptr].wrapping_sub(1),
-                Cmd::Left => ptr = ptr.checked_sub(1).ok_or_else(|| Error::PointerOutOfBoundsError('<'))?,
-                Cmd::Right => ptr = Some(ptr + 1).filter(|&x| x < MEMORY_SIZE).ok_or_else(|| Error::PointerOutOfBoundsError('>'))?,
-                Cmd::Getc => if let Some(byte) = getc(input)? { memory[ptr] = byte; },
+                Cmd::Left => {
+                    ptr = ptr
+                        .checked_sub(1)
+                        .ok_or(Error::PointerOutOfBoundsError('<'))?
+                }
+                Cmd::Right => {
+                    ptr = Some(ptr + 1)
+                        .filter(|&x| x < MEMORY_SIZE)
+                        .ok_or(Error::PointerOutOfBoundsError('>'))?
+                }
+                Cmd::Getc => {
+                    if let Some(byte) = getc(input)? {
+                        memory[ptr] = byte;
+                    }
+                }
                 Cmd::Putc => putc(output, memory[ptr])?,
             }
         }
@@ -43,7 +69,12 @@ pub fn run<I: BufRead, O: Write>(source: &str, input: &mut I, output: &mut O) ->
 
 #[derive(Debug, Clone, Copy)]
 enum Cmd {
-    Inc, Dec, Left, Right, Getc, Putc,
+    Inc,
+    Dec,
+    Left,
+    Right,
+    Getc,
+    Putc,
 }
 
 #[derive(Debug)]
@@ -84,7 +115,7 @@ fn into_basic_blocks(source: &str) -> Result<ByteCodeProgram, Error> {
             ']' => {
                 // starts next basic block
                 // jz target is bb+1; jnz target is popped+1; jz of popped is bb+1
-                let popped = bbno_stack.pop().ok_or_else(|| Error::SyntaxError(']'))?;
+                let popped = bbno_stack.pop().ok_or(Error::SyntaxError(']'))?;
                 let bb = BasicBlock {
                     instrs: cur_basic_block,
                     jz: Some(cur_bb_id + 1),
@@ -95,11 +126,11 @@ fn into_basic_blocks(source: &str) -> Result<ByteCodeProgram, Error> {
                 cur_bb_id += 1;
                 cur_basic_block = vec![];
             }
-            _ => ()
+            _ => (),
         }
     }
     if !bbno_stack.is_empty() {
-        Err(Error::SyntaxError('['))?
+        return Err(Error::SyntaxError('['));
     }
     let bb = BasicBlock {
         instrs: cur_basic_block,
@@ -118,6 +149,86 @@ fn getc<I: BufRead>(input: &mut I) -> Result<Option<u8>, Error> {
 }
 
 fn putc<O: Write>(output: &mut O, byte: u8) -> Result<(), Error> {
-    output.write(&[byte][..])?;
+    output.write_all(&[byte][..])?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::BufReader;
+
+    use super::*;
+
+    #[test]
+    fn test_printer_1() {
+        // from https://codegolf.stackexchange.com/a/108062/78410
+        let code = "
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        >>>>>>>>
+        +[[-<]-[->]<-]<.<<<<.>>>>-.<<-.<.>>.<<<+++.>>>---.<++.";
+        let mut stdin = BufReader::new(&b""[..]);
+        let mut stdout: Vec<u8> = vec![];
+        let res = run(code, &mut stdin, &mut stdout);
+        assert!(res.is_ok());
+        assert_eq!(stdout, b"brainfuck");
+    }
+
+    #[test]
+    fn test_printer_2() {
+        // from https://codegolf.stackexchange.com/a/99016/78410
+        let code = "++[>+++++<-]++[>>>+++++<<<-]++++++[>>+++++++<<-]>[>..........>.<<-]";
+        let mut stdin = BufReader::new(&b""[..]);
+        let mut stdout: Vec<u8> = vec![];
+        let res = run(code, &mut stdin, &mut stdout);
+        assert!(res.is_ok());
+        assert_eq!(stdout, b"**********\n**********\n**********\n**********\n**********\n**********\n**********\n**********\n**********\n**********\n");
+    }
+
+    #[test]
+    fn test_io_1() {
+        // from https://codegolf.stackexchange.com/a/210478/78410
+        let code = ">+[,>++++[<-------->-]<]>++++[<++++++++>-]<[>,]++++[<-------->-]<[[-]++++[<-------->-]<]<[<]>>[.>]";
+        let testcases = [
+            &b"Samantha Vee Hills"[..],
+            b"Bob Dillinger",
+            b"John Jacob Jingleheimer Schmidt",
+            b"Jose Mario Carasco-Williams",
+            b"James Alfred Van Allen",
+        ];
+        let outputs = [
+            &b"Vee"[..],
+            b"",
+            b"Jacob Jingleheimer",
+            b"Mario",
+            b"Alfred Van",
+        ];
+        for (testcase, expected) in testcases.into_iter().zip(outputs) {
+            let mut stdin = BufReader::new(testcase);
+            let mut stdout: Vec<u8> = vec![];
+            let res = run(code, &mut stdin, &mut stdout);
+            assert!(res.is_ok());
+            assert_eq!(stdout, expected);
+        }
+    }
+
+    #[test]
+    fn test_io_2() {
+        // from https://codegolf.stackexchange.com/a/172401/78410
+        let code = ">>>>>>>-[-[-<]>>+<]>-<<+[[[-]<,[->+>+<<]>[-<+>]>>[-<->>+<]<]<<[>>+<<[-]]<[<]>[[.>]<[<]>[-]>]>>>>.<<]";
+        let testcases = [&b"laser bat "[..], b"on the topic of existence "];
+        let outputs = [
+            &b"laserasersererr batatt "[..],
+            b"onn thehee topicopicpicicc off existencexistenceistencestencetenceencencecee ",
+        ];
+        for (testcase, expected) in testcases.into_iter().zip(outputs) {
+            let mut stdin = BufReader::new(testcase);
+            let mut stdout: Vec<u8> = vec![];
+            let res = run(code, &mut stdin, &mut stdout);
+            dbg!(&res);
+            assert!(matches!(res, Err(Error::PointerOutOfBoundsError('>'))));
+            assert_eq!(stdout, expected);
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,21 @@
+//! Rust (re-)implementations of various esolangs
+//!
+//! Includes implementations of various esolangs through a unified interface.
+//! Since many languages have commands for I/O side effects, each implementation
+//! takes input and output streams as parameters in addition to the source code:
+//!
+//! ```ignore
+//! pub fn run<I: BufRead, O: Write>(source: &str, input: &mut I, output: &mut O) -> Result<(), Error>
+//! ```
+//!
+//! This `run` function returns `Ok(())` if run successfully, and `Err(...)` if
+//! the program was terminated by some kind of error. The `Error` enum is unique
+//! to each language, containing all possible error situations. Refer to the
+//! respective docs for details.
+//!
+//! Each language implementation is intended to be "faster than naive",
+//! which will often be achieved by compiling "halfway" to bytecode.
+
+#![warn(missing_docs)]
+
 pub mod brainfuck;
