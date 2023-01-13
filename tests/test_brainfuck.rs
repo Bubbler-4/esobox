34847bce0d8b1d464c5201a634dd2c819a59ee91
Bubbler-4 <bubbler9903@gmail.com>
2023-01-13 07:53:11 +0000

    Start moving bf tests

---
+++ tests/test_brainfuck.rs
@@ -0,0 +1,17 @@
+use std::process::{Command, Stdio};
+
+fn esobox() -> Command {
+    test_bin::get_test_bin("esobox")
+}
+
+#[test]
+fn integration_print_bf() {
+    let mut esobox = esobox();
+    let lang = "brainfuck";
+    let source = "tests/brainfuck/print_bf.bf";
+    let output = esobox.args([lang, source]).stdin(Stdio::null()).output().expect("Failed to run process");
+    let stdout = String::from_utf8(output.stdout).expect("Output is not valid UTF-8");
+    let stderr = String::from_utf8(output.stderr).expect("Output is not valid UTF-8");
+    assert_eq!(stdout, "brainfuck");
+    assert!(stderr.is_empty());
+}
